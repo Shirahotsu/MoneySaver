8b3f3d96e6f97b2244fe8a44950ac76c0765fa8e
Arek <arek.erduk@gmail.com>
2019-08-10 15:03:34 +0200

    Summary expenses

---
+++ src/components/StartScreen/ExpensesSummary.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-card>
+    <q-card-section v-if="expensesData.header" class="flex row col-12 justify-around">
+  {{expensesData.header}}
+    </q-card-section>
+    <q-separator v-if="expensesData.header" />
+    <q-card-section class="flex row col-12 justify-around">
+      <div v-for="(item, index) in expensesData.array" :key="index" class="fex column justify-around">
+        <div class="row justify-center q-pb-sm">{{item.key}}</div>
+        <div class="row justify-center">{{item.value}}</div>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  props: {
+    expensesData: Object
+  }
+}
+</script>
+
+<style scoped>
+.v-separator{
+  width: 1px;
+  background-color: #000;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,7 @@
 <template>
-  <q-page class="flex flex-center">
-    TEST
+  <q-page padding>
+    <ExpensesSummary class="q-my-lg" :expensesData = averageExpenses></ExpensesSummary>
+    <ExpensesSummary class="q-my-lg" :expensesData = favoriteExpenses></ExpensesSummary>
   </q-page>
 </template>
 
@@ -8,7 +9,23 @@
 </style>
 
 <script>
+import ExpensesSummary from '../components/StartScreen/ExpensesSummary'
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: {
+    ExpensesSummary: ExpensesSummary
+  },
+  data () {
+    return {
+      averageExpenses: {
+        header: 'Średnie wydatki',
+        array: [{ key: 'Dzienne', value: '30' }, { key: 'Tygodniowe', value: '200' }]
+      },
+      favoriteExpenses: {
+        header: null,
+        array: [{ key: 'Jedzenie', value: '30' }, { key: 'Alkochol', value: '200' }]
+      }
+    }
+  }
 }
 </script>
